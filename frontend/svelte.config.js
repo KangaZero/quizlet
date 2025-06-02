39822c77d80d117d8ac3942ac25f2df8bf0d565f
KangaZero <samuelyongw@gmail.com>
2025-06-02 16:40:26 +0900

    added vercel configuration

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,19 +1,19 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		appDir: 'app',
 		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
+			// pages: 'build',
+			// assets: 'build',
 			fallback: '404.html',
-			precompress: false,
+			// precompress: false,
 			strict: true
 		}),
-		paths: {
-			base: process.argv.includes('dev') ? '' : '/quizlet'
-		}
+		// paths: {
+		// 	base: process.argv.includes('dev') ? '' : '/quizlet'
+		// }
 	}
 };
 
